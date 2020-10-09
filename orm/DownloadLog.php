31fcace2d5001440bd3e4d25adebf28d54faeb17
Flinn G <flinnja@gmail.com>
2020-10-09 17:25:44 +1300

    create DownloadLog_validator_test.php, fulfil it by creating NumericValidator.php and using this trait to validate the set methods of DownloadLog.php

--- orm/DownloadLog.php
+++ orm/DownloadLog.php
@@ -3,45 +3,49 @@
 namespace Orm;
 
 include_once('ActiveRecord.php');
+include_once('../traits/NumericValidator.php');
+use Traits\NumericValidator;
 
 final class DownloadLog extends ActiveRecord
 {
-    /* @var int */
-    private $fileId;
+	use NumericValidator;
 
-    /* @var int */
-    private $userId;
+  /* @var int */
+  private $fileId;
 
-    public function isModified():bool{
-    	return $this->isModified;
-    }
+  /* @var int */
+  private $userId;
 
-    public function create(){
-    	return new DownloadLog();
-    }
+  public function isModified():bool{
+  	return $this->isModified;
+  }
 
-    public function __destruct(){
-    	echo ('Destroying DownloadLog');
-    }
+  public function create(){
+  	return new DownloadLog();
+  }
 
-    public function setFileId($id){
-    	$this->$fileId = $id;
-    	$this->isModified = true;
-    	return $this;
-    }
+  public function __destruct(){
+  	echo ('Destroying DownloadLog');
+  }
 
-    public function setUserId($id){
-    	$this->$userId = $id;
-    	$this->isModified = true;
-    	return $this;
-    }
+  public function setFileId($id){
+  	$this->$fileId = $this->validateNumeric($id);
+  	$this->isModified = true;
+  	return $this;
+  }
 
-    //not tested so not techically required but seems like a sensible method to add
-    public function getFileId(){
-    	return $this->$fileId;
-    }
+  public function setUserId($id){
+  	$this->$userId = $this->validateNumeric($id);
+  	$this->isModified = true;
+  	return $this;
+  }
 
-    public function getUserId(){
-    	return $this->$userId;
-    }
+  //not tested so not techically required but seems like a sensible method to add
+  public function getFileId(){
+  	return $this->$fileId;
+  }
+
+  public function getUserId(){
+  	return $this->$userId;
+  }
 }
---
+++ task-1-coding-challenges.md
@@ -0,0 +1,3 @@
+#Coding Challenges
+##Code Challenge 1
+Running a php server and serving `/tests/DownloadLog_test.php` should display the correct output in your browser for code challenge one.
